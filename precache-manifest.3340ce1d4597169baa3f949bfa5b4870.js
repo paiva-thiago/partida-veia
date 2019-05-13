20bf7a07e7f81b1e08898b888d052b1a4714b6fa
Thiago Paiva <paiva.thiago@gmail.com>
2019-05-13 13:53:22 -0300

    Updates

---
+++ precache-manifest.3340ce1d4597169baa3f949bfa5b4870.js
@@ -0,0 +1,462 @@
+self.__precacheManifest = [
+  {
+    "revision": "87a076f9d74eb9148b985b242b29bccb",
+    "url": "/partida-veia/static/media/keys.87a076f9.ico"
+  },
+  {
+    "revision": "01332ce6e6206606524d",
+    "url": "/partida-veia/static/css/main.b0fde922.chunk.css"
+  },
+  {
+    "revision": "8946938577ba03d212bb",
+    "url": "/partida-veia/static/js/runtime~main.a8272c4d.js"
+  },
+  {
+    "revision": "39ba35646ff2534b4479",
+    "url": "/partida-veia/static/js/2.543c8b62.chunk.js"
+  },
+  {
+    "revision": "91b9ca5534c58920de33dc76e57008c3",
+    "url": "/partida-veia/static/media/arrow_left.91b9ca55.ico"
+  },
+  {
+    "revision": "aad12aaedb583e394170479a82fdc09f",
+    "url": "/partida-veia/static/media/arrow_right.aad12aae.ico"
+  },
+  {
+    "revision": "f04f026c29066ec3459a4218a9b2647b",
+    "url": "/partida-veia/static/media/bat.f04f026c.ico"
+  },
+  {
+    "revision": "8bb01c24b328e5f079505ba9f7e68cd6",
+    "url": "/partida-veia/static/media/bat_exec.8bb01c24.ico"
+  },
+  {
+    "revision": "51e66ed075bf5c0f61719741a77e8af1",
+    "url": "/partida-veia/static/media/bat_exec_2.51e66ed0.ico"
+  },
+  {
+    "revision": "a4c7b3fb670c20962eb6b29e0cdc0d77",
+    "url": "/partida-veia/static/media/bat_wait.a4c7b3fb.ico"
+  },
+  {
+    "revision": "b35ecca2c0289b147883fae21e021548",
+    "url": "/partida-veia/static/media/bookmark.b35ecca2.ico"
+  },
+  {
+    "revision": "beb0f6b87c81a32971e4e69404f2937e",
+    "url": "/partida-veia/static/media/brush.beb0f6b8.ico"
+  },
+  {
+    "revision": "acd697cda506baad1319901df553ae94",
+    "url": "/partida-veia/static/media/bulb.acd697cd.ico"
+  },
+  {
+    "revision": "d9250f9aa8c2f6595af4a6ac55064f09",
+    "url": "/partida-veia/static/media/calculator.d9250f9a.ico"
+  },
+  {
+    "revision": "8236e87d636bd61d870f1edb210cfb20",
+    "url": "/partida-veia/static/media/camera.8236e87d.ico"
+  },
+  {
+    "revision": "940ee8339c7e4c9eeed34086f7cb87e1",
+    "url": "/partida-veia/static/media/computer.940ee833.ico"
+  },
+  {
+    "revision": "b0b7a6c591b8a133e5528ad07bb8630e",
+    "url": "/partida-veia/static/media/computer_2.b0b7a6c5.ico"
+  },
+  {
+    "revision": "bea0c092c98ae34633543038d471098e",
+    "url": "/partida-veia/static/media/computer_3.bea0c092.ico"
+  },
+  {
+    "revision": "8683e84be9ba48c32daf5604877c77d9",
+    "url": "/partida-veia/static/media/computer_4.8683e84b.ico"
+  },
+  {
+    "revision": "098324443574776f0c5157a0ebc1a8a6",
+    "url": "/partida-veia/static/media/computer_find.09832444.ico"
+  },
+  {
+    "revision": "14be2b381969ab5f20564e67d122fc6f",
+    "url": "/partida-veia/static/media/defrag.14be2b38.ico"
+  },
+  {
+    "revision": "0a839ae908a466962dfc528533a9c9f6",
+    "url": "/partida-veia/static/media/desktop.0a839ae9.ico"
+  },
+  {
+    "revision": "b51938a521204f5eed481ec325a58ad1",
+    "url": "/partida-veia/static/media/earth.b51938a5.ico"
+  },
+  {
+    "revision": "a59dcff48e7fe14d6ed7ca77a854d273",
+    "url": "/partida-veia/static/media/explore.a59dcff4.ico"
+  },
+  {
+    "revision": "7863d8dcd2f5a8f6db176f931ad229ca",
+    "url": "/partida-veia/static/media/file_delete.7863d8dc.ico"
+  },
+  {
+    "revision": "dfc610d4ab042b443372b6f128265b69",
+    "url": "/partida-veia/static/media/file_find.dfc610d4.ico"
+  },
+  {
+    "revision": "25fe37fd19e9cd0e67ec0431bea2751a",
+    "url": "/partida-veia/static/media/file_find2.25fe37fd.ico"
+  },
+  {
+    "revision": "134b2bda8fdd7ce1b5a274b27a3095e7",
+    "url": "/partida-veia/static/media/file_find3.134b2bda.ico"
+  },
+  {
+    "revision": "9d92400703f9db6e1b967b1c5901446e",
+    "url": "/partida-veia/static/media/file_corrupted.9d924007.ico"
+  },
+  {
+    "revision": "d2cabc7b979e499c28b94c4caed50904",
+    "url": "/partida-veia/static/media/file_font.d2cabc7b.ico"
+  },
+  {
+    "revision": "9362af07b93871b848aa664f40230b84",
+    "url": "/partida-veia/static/media/file_font_2.9362af07.ico"
+  },
+  {
+    "revision": "2f14504ce2de18681b33045e2f51a1f9",
+    "url": "/partida-veia/static/media/file_pen.2f14504c.ico"
+  },
+  {
+    "revision": "88c777d542a1bb380c84cb4b3101d990",
+    "url": "/partida-veia/static/media/file_pencil.88c777d5.ico"
+  },
+  {
+    "revision": "2c553e266f9d65ecf4923fd7d59a97ed",
+    "url": "/partida-veia/static/media/file_pin.2c553e26.ico"
+  },
+  {
+    "revision": "f0d1ae3acfb1d8a38cbab6c194eca363",
+    "url": "/partida-veia/static/media/file_settings.f0d1ae3a.ico"
+  },
+  {
+    "revision": "fcb4db91e0863d282c9f8fb647970798",
+    "url": "/partida-veia/static/media/file_text.fcb4db91.ico"
+  },
+  {
+    "revision": "ec9aa8218032f22a23cbee02c8e5cac8",
+    "url": "/partida-veia/static/media/file_text_settings.ec9aa821.ico"
+  },
+  {
+    "revision": "0beb86508b618f2bcd8988b3db04c3d0",
+    "url": "/partida-veia/static/media/file_transfer.0beb8650.ico"
+  },
+  {
+    "revision": "e69b9c0f2b698b24bfbed4cb7cec8a2f",
+    "url": "/partida-veia/static/media/files.e69b9c0f.ico"
+  },
+  {
+    "revision": "cd6da24756189f924a90d813fc047efe",
+    "url": "/partida-veia/static/media/folder.cd6da247.ico"
+  },
+  {
+    "revision": "e5028c51f82ffcb8af9109af953ade50",
+    "url": "/partida-veia/static/media/folder_exe.e5028c51.ico"
+  },
+  {
+    "revision": "d08005f7b6ff68526ef3d6f1a62e1e66",
+    "url": "/partida-veia/static/media/folder_exe2.d08005f7.ico"
+  },
+  {
+    "revision": "00555c56910078986436ce9a235129b5",
+    "url": "/partida-veia/static/media/folder_file.00555c56.ico"
+  },
+  {
+    "revision": "7d1940dd8a645bd40eaadfd8920b72d2",
+    "url": "/partida-veia/static/media/folder_font.7d1940dd.ico"
+  },
+  {
+    "revision": "2a6d646ec292dadd0a1224827eb9e689",
+    "url": "/partida-veia/static/media/folder_open.2a6d646e.ico"
+  },
+  {
+    "revision": "63ce4f02aab9c670dc1ef4800a1f597b",
+    "url": "/partida-veia/static/media/folder_print.63ce4f02.ico"
+  },
+  {
+    "revision": "9ef45a1d03c85f7d29443652c2afb04a",
+    "url": "/partida-veia/static/media/folder_rename.9ef45a1d.ico"
+  },
+  {
+    "revision": "a2a910bc575c7bf861321edf675218e9",
+    "url": "/partida-veia/static/media/folder_settings.a2a910bc.ico"
+  },
+  {
+    "revision": "e9e02b1debd453ed56813d1350217ece",
+    "url": "/partida-veia/static/media/folder_settings_2.e9e02b1d.ico"
+  },
+  {
+    "revision": "c433c615ce7c78f7a3c6727f25848015",
+    "url": "/partida-veia/static/media/folder_shared.c433c615.ico"
+  },
+  {
+    "revision": "d35443fdaef42408fa37e590e1e0402e",
+    "url": "/partida-veia/static/media/forbidden.d35443fd.ico"
+  },
+  {
+    "revision": "39a48327a218570089b209fcefb10abc",
+    "url": "/partida-veia/static/media/globe.39a48327.ico"
+  },
+  {
+    "revision": "b0a73d9a5d8260c62d3c0d084870b119",
+    "url": "/partida-veia/static/media/hand.b0a73d9a.ico"
+  },
+  {
+    "revision": "5a264a05c2fee3a71cd8b62ce74685c8",
+    "url": "/partida-veia/static/media/help_book.5a264a05.ico"
+  },
+  {
+    "revision": "b7358be8ad588e6581b0723fdb157137",
+    "url": "/partida-veia/static/media/issue.b7358be8.ico"
+  },
+  {
+    "revision": "c0f3c2ffe1af115bb8b665ff07d0fa71",
+    "url": "/partida-veia/static/media/install.c0f3c2ff.ico"
+  },
+  {
+    "revision": "242fe10c283af476fed27da31dcbe0b6",
+    "url": "/partida-veia/static/media/key.242fe10c.ico"
+  },
+  {
+    "revision": "01332ce6e6206606524d",
+    "url": "/partida-veia/static/js/main.319d862b.chunk.js"
+  },
+  {
+    "revision": "55d0c4548ae3112497de8984aed0a862",
+    "url": "/partida-veia/static/media/loader_bat.55d0c454.ico"
+  },
+  {
+    "revision": "64566de6d4603f8f0c5de45d2b1fcda8",
+    "url": "/partida-veia/static/media/lock.64566de6.ico"
+  },
+  {
+    "revision": "434131cf5351e343bff0236cac601751",
+    "url": "/partida-veia/static/media/logo.434131cf.ico"
+  },
+  {
+    "revision": "f658dd415baa59383b6c6127c0b6fb2c",
+    "url": "/partida-veia/static/media/mail.f658dd41.ico"
+  },
+  {
+    "revision": "c912f976aa0c363550093f7890793995",
+    "url": "/partida-veia/static/media/media_cd.c912f976.ico"
+  },
+  {
+    "revision": "5e4b3c0789fd37c93f1927c4af67768c",
+    "url": "/partida-veia/static/media/memory.5e4b3c07.ico"
+  },
+  {
+    "revision": "070f067c8067d7f867bd429173efdb66",
+    "url": "/partida-veia/static/media/mic.070f067c.ico"
+  },
+  {
+    "revision": "924ccc5ecb8c17415d955f919605fa3a",
+    "url": "/partida-veia/static/media/mute.924ccc5e.ico"
+  },
+  {
+    "revision": "ef8580f61644eff7c837ee768880bc33",
+    "url": "/partida-veia/static/media/network.ef8580f6.ico"
+  },
+  {
+    "revision": "a749d18e64dc30d9b30be6952fa512f7",
+    "url": "/partida-veia/static/media/network_2.a749d18e.ico"
+  },
+  {
+    "revision": "83b3f2e56fc27415c141c2c6f2b23df3",
+    "url": "/partida-veia/static/media/network_3.83b3f2e5.ico"
+  },
+  {
+    "revision": "b4ace140897385807eb1ee3032eefae6",
+    "url": "/partida-veia/static/media/notepad.b4ace140.ico"
+  },
+  {
+    "revision": "9634c307420b029a0e2a033685a990c0",
+    "url": "/partida-veia/static/media/packager.9634c307.ico"
+  },
+  {
+    "revision": "d01c4d0246250429d09f734732dbeda9",
+    "url": "/partida-veia/static/media/phone.d01c4d02.ico"
+  },
+  {
+    "revision": "b02edd960bf797a498a5dc1d3a94bc47",
+    "url": "/partida-veia/static/media/plugin.b02edd96.ico"
+  },
+  {
+    "revision": "55d7a09c99272dee38dee99dc03d85d9",
+    "url": "/partida-veia/static/media/power_off.55d7a09c.ico"
+  },
+  {
+    "revision": "1d41412360d3e51bb84b4b99cb16d32b",
+    "url": "/partida-veia/static/media/power_on.1d414123.ico"
+  },
+  {
+    "revision": "861d477f9cc68c6f7a57ecf461c16f97",
+    "url": "/partida-veia/static/media/printer.861d477f.ico"
+  },
+  {
+    "revision": "654b1b6eafbcb61c9972eea6d988fb14",
+    "url": "/partida-veia/static/media/printer_calendar.654b1b6e.ico"
+  },
+  {
+    "revision": "c1fc0be2528f3420914d15480a331bb4",
+    "url": "/partida-veia/static/media/printer_drive.c1fc0be2.ico"
+  },
+  {
+    "revision": "0a02efef03add1369bd6850b7413a271",
+    "url": "/partida-veia/static/media/printer_shared.0a02efef.ico"
+  },
+  {
+    "revision": "884102e696703574251a969bf8a69630",
+    "url": "/partida-veia/static/media/reader_cd.884102e6.ico"
+  },
+  {
+    "revision": "39bae689ff8f9adc53a7fb65517f3fcf",
+    "url": "/partida-veia/static/media/reader_cd_2.39bae689.ico"
+  },
+  {
+    "revision": "e52a7df7d955920ce69238527f5b6911",
+    "url": "/partida-veia/static/media/reader_closed.e52a7df7.ico"
+  },
+  {
+    "revision": "2942ebb8da31072221cc33056d7effa6",
+    "url": "/partida-veia/static/media/reader_disket.2942ebb8.ico"
+  },
+  {
+    "revision": "6cd9b6f948d0aa1d0c170220fc57ca85",
+    "url": "/partida-veia/static/media/reader_disket_casset.6cd9b6f9.ico"
+  },
+  {
+    "revision": "6121f75ce0ec24f8406b3125a0274bdc",
+    "url": "/partida-veia/static/media/reader_disket2.6121f75c.ico"
+  },
+  {
+    "revision": "676dfff7a67390a7509911ab7f830c4f",
+    "url": "/partida-veia/static/media/reader_eject.676dfff7.ico"
+  },
+  {
+    "revision": "53ba63332f1e3365742042e012740d07",
+    "url": "/partida-veia/static/media/reader_noshared.53ba6333.ico"
+  },
+  {
+    "revision": "43ef80d0b71d9fdd2074ab0fb77e2339",
+    "url": "/partida-veia/static/media/reader_opened.43ef80d0.ico"
+  },
+  {
+    "revision": "5c1e61964c4cd4293d3cd2886d3e2666",
+    "url": "/partida-veia/static/media/reader_shared.5c1e6196.ico"
+  },
+  {
+    "revision": "d8c0936b80408f4791f2488d33deb7f9",
+    "url": "/partida-veia/static/media/regedit.d8c0936b.ico"
+  },
+  {
+    "revision": "a87239ea46f2104b79319a21e82b7439",
+    "url": "/partida-veia/static/media/recycle_empty.a87239ea.ico"
+  },
+  {
+    "revision": "b307565cb0bb6940ebddca3d751a4666",
+    "url": "/partida-veia/static/media/recycle_file.b307565c.ico"
+  },
+  {
+    "revision": "5ecaa98fed395361ce50819c7b24ec4c",
+    "url": "/partida-veia/static/media/recycle_filefolder.5ecaa98f.ico"
+  },
+  {
+    "revision": "3aa560e3704d1c9636d31b32573833d7",
+    "url": "/partida-veia/static/media/recycle_folder.3aa560e3.ico"
+  },
+  {
+    "revision": "2452d88f6e14404f6cfd0ccaa5f3dfa0",
+    "url": "/partida-veia/static/media/recycle_full.2452d88f.ico"
+  },
+  {
+    "revision": "6c1c5ff08eb0f3d84c537b6e8174a789",
+    "url": "/partida-veia/static/media/settings.6c1c5ff0.ico"
+  },
+  {
+    "revision": "c6e2ef0058800c36c5c27ec38f0582f3",
+    "url": "/partida-veia/static/media/shortcut.c6e2ef00.ico"
+  },
+  {
+    "revision": "8780b62be2ac56967459d3bf79dcab09",
+    "url": "/partida-veia/static/media/shortcut2.8780b62b.ico"
+  },
+  {
+    "revision": "b16e0fc3dee3ec1dcde58455bf9526bd",
+    "url": "/partida-veia/static/media/star.b16e0fc3.ico"
+  },
+  {
+    "revision": "b67631ddf52e653c6ee5fcbfc393b02a",
+    "url": "/partida-veia/static/media/textchat.b67631dd.ico"
+  },
+  {
+    "revision": "bba095c8991d4e5c7a788f50b4bbdc7f",
+    "url": "/partida-veia/static/media/textchat_2.bba095c8.ico"
+  },
+  {
+    "revision": "c82ec7be3376022d6e4b17781fa51d9b",
+    "url": "/partida-veia/static/media/timedate.c82ec7be.ico"
+  },
+  {
+    "revision": "1f9758a4709e44b8f951f3a5eeb5e0d3",
+    "url": "/partida-veia/static/media/tree.1f9758a4.ico"
+  },
+  {
+    "revision": "c0316afeeba3a715f5f3eb506a6a960b",
+    "url": "/partida-veia/static/media/unmute.c0316afe.ico"
+  },
+  {
+    "revision": "286a4dbbb1a138426c22a3e1821600ab",
+    "url": "/partida-veia/static/media/user.286a4dbb.ico"
+  },
+  {
+    "revision": "121535ab60b58069d93008baf444be00",
+    "url": "/partida-veia/static/media/warning.121535ab.ico"
+  },
+  {
+    "revision": "1317fe440a26aadadf30d94edd5a9525",
+    "url": "/partida-veia/static/media/wordpad.1317fe44.ico"
+  },
+  {
+    "revision": "dc858fcb488db3d22e3083eadc95bc5f",
+    "url": "/partida-veia/static/media/windows_explorer.dc858fcb.ico"
+  },
+  {
+    "revision": "84f6caef1b3a28913611bd84b0b952da",
+    "url": "/partida-veia/static/media/ms_dos.84f6caef.ico"
+  },
+  {
+    "revision": "caad6267a1610224531fc95bc8643d21",
+    "url": "/partida-veia/static/media/microsoft_network.caad6267.ico"
+  },
+  {
+    "revision": "035228706feccc9007635f0f8e22743a",
+    "url": "/partida-veia/static/media/microsoft_exchange.03522870.ico"
+  },
+  {
+    "revision": "8e6929ee77cc008e1e197e27f00c8339",
+    "url": "/partida-veia/static/media/check.8e6929ee.svg"
+  },
+  {
+    "revision": "29c235007007d931220e07475f3c0a88",
+    "url": "/partida-veia/static/media/check-disabled.29c23500.svg"
+  },
+  {
+    "revision": "274443ac2f681dd2045f40bef229492a",
+    "url": "/partida-veia/static/media/rightcaret.274443ac.svg"
+  },
+  {
+    "revision": "34c9b38fb8ec74095cebfc56fc43b9e6",
+    "url": "/partida-veia/index.html"
+  }
+];
